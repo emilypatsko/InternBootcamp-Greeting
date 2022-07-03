7fa3e698baaae970b579b93c7e1859b90e0377f8
Emily Patsko <emily.patsko@softwire.com>
2022-07-03 15:16:36 +0100

    Ask user for first name and surname

--- Greeting/Program.cs
+++ Greeting/Program.cs
@@ -4,9 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there! What is your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}. Nice to meet you!");
+            Console.WriteLine("Hello there! What is your first name?");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("And what is your surname?");
+            var lastName = Console.ReadLine();
+            if (String.IsNullOrEmpty(lastName))
+            {
+                if (String.IsNullOrEmpty(firstName))
+                {
+                    Console.WriteLine("Well, that's just not very helpful, is it.");
+                    return;
+                }
+                Console.WriteLine($"Hello, {firstName}! I see you only have one name, but that's fine!");
+                return;
+            }
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
         }
     }
 }
