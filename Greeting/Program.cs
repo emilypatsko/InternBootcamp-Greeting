251bf005296c37b7fc86f2f4ca5d9fadc54b8ec2
Emily Patsko <emily.patsko@softwire.com>
2022-07-03 15:12:09 +0100

    Create Greeting console application

---
+++ Greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there! What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}. Nice to meet you!");
+        }
+    }
+}
